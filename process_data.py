0ffb187030e0ed283db4ea48e7b93aadcb4b2708
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2021-11-11 21:06:56 +0000

    import relevant packages

--- process_data.py
+++ process_data.py
@@ -1,1 +1,11 @@
+import pandas as pd
+from bokeh.plotting import figure, output_notebook, show
+#from bokeh.plotting import reset_output
 
+from IPython.display import display, clear_output
+
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
+
+from astropy import constants as const
+from astropy import units as u
