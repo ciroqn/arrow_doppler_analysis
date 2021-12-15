7d0dd4e87eefb27e30e080c08a3abb2fb3da27df
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2021-12-15 20:04:07 +0000

    plotting rotation curve after calculations

--- rotation_curve.py
+++ rotation_curve.py
@@ -73,3 +73,51 @@ import pandas as pd
 
 %matplotlib inline
 output_notebook()
+
+
+##################################### PLOTTING ROTATION CURVE (I.E. ORBITAL VELOCITY VS RADIUS (IN KPC)) ########################################
+# Plot the 10 data points for ROTATION CURVE
+
+x = df['radius (kpc)']
+y = df['velocity (km/s)']
+y_err = df['v_error (km/s)']
+
+plt.plot(x,y, '.',label='Data Points')
+plt.errorbar(x,y,yerr=y_err, xerr=None, fmt='none')
+plt.ylim(0,300)
+plt.ylabel('Velocity / kms\u207b\u00b9')
+plt.xlabel('Radius / kpc')
+plt.grid(False)
+plt.legend()
+
+# no need for 'plt.show()' until after the code for curve fit
+
+
+# Alternative to the scipy line fit.
+#x = np.linspace(0,8.5,100)
+#y = 250-197*np.exp(-1.00*x)
+#plt.plot(x, y, '-r', label='Eq')
+
+# Fitting line to graph
+from scipy.optimize import curve_fit
+
+# Here'e a simple mathematical curve that might fit
+def gal_curve(r,C0,C1,C2):
+    return C0-C1*np.exp(-C2*r)
+
+R = df['radius (kpc)'].values
+V = df['velocity (km/s)'].values
+R2 = np.linspace(0,8.5, 50)
+
+# Use some trial values for initial parameters for the constants
+p0 = [250,200,0.15]
+
+# and fit the curve 
+c,pcov = curve_fit(gal_curve,R,V,p0)  # curvue_fit(function, x, y, parameters)
+
+print('Optimised parameters are:', *c)
+
+# Plot it 
+plt.plot(R2,gal_curve(R2,*c), label='Galactic Rotation Curve')   # plt.plot(x,y)
+plt.legend()
+plt.show()
