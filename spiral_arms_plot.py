7d01e54dd7c549bfb0684612ef85625827a5e11f
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2021-12-15 20:18:08 +0000

    sift through data to separate pos and neg vels

--- spiral_arms_plot.py
+++ spiral_arms_plot.py
@@ -53,3 +53,86 @@ df.head(40)
 #df.tail(40)
 
 
+##################### SEPARATING REDSHIFTED AND BLUESHIFTED CLOUDS TO PLOT ON POLAR GRAPH (COLOUR-CODED) #######################################
+
+# To separate neg velocities from the rest
+index_neg_vel = []
+neg_vels = []
+for index, vel in enumerate(df['v_obs (km/s)']):    # the enumerate() function gives the index of the velocity that satisfies the condition directly below
+    if vel < 0:
+        index_neg_vel.append(index)
+        neg_vels.append(vel)
+
+radius_sun_km = []
+relevant_longitudes = []
+for ind in index_neg_vel:
+    radius_neg_vel = df.iloc[ind, 2]
+    longitudes_neg_vel = df.iloc[ind, 0]
+    radius_sun_km.append(radius_neg_vel)
+    relevant_longitudes.append(longitudes_neg_vel)
+
+rad_sun_km_array = np.array(radius_sun_km)
+rel_long_array = np.array(relevant_longitudes)
+
+sin_l_neg = np.sin(rel_long_array*(np.pi/180))
+
+cos_l_neg = np.cos(rel_long_array*(np.pi/180))
+first_part_neg = R0_KM*cos_l_neg
+discriminant = (rad_sun_km_array)**2-(R0_KM**2)*(sin_l_neg)**2
+sqrt_neg = (discriminant)**0.5
+R1_neg = first_part_neg+sqrt_neg
+# Now for R1_neg in kpc
+R_neg_kpc = R1_neg / KPC_KM
+
+# Separate positive velocities
+index_pos_vel = []
+pos_vels = []
+for index, vel in enumerate(df['v_obs (km/s)']):
+    if vel > 0:
+        index_pos_vel.append(index)
+        pos_vels.append(vel)
+        
+radius_sun_km_pos = []
+relevant_longitudes_pos = []
+for ind in index_pos_vel:
+    radius_pos_vel = df.iloc[ind, 2]
+    longitudes_pos_vel = df.iloc[ind, 0]
+    radius_sun_km_pos.append(radius_pos_vel)
+    relevant_longitudes_pos.append(longitudes_pos_vel)
+        
+rad_sun_km_pos_array = np.array(radius_sun_km_pos)
+rel_long_array_pos = np.array(relevant_longitudes_pos)
+
+sin_l_pos = np.sin(rel_long_array_pos*(np.pi/180))
+
+cos_l_pos = np.cos(rel_long_array_pos*(np.pi/180))
+first_part_pos = R0_KM*cos_l_pos
+discriminant = (rad_sun_km_pos_array)**2-(R0_KM**2)*(sin_l_pos)**2
+sqrt_pos = (discriminant)**0.5
+R1_pos = first_part_pos+sqrt_pos
+# Now for R1_neg in kpc
+R_pos_kpc = R1_pos / KPC_KM
+
+
+# Plot polar curve with neg and pos velocities
+cloud_thetas_pos = rel_long_array_pos*(np.pi/180)       
+cloud_thetas_neg = rel_long_array*(np.pi/180)  
+
+plot = plt.subplot(111, projection = 'polar')
+plt.scatter(cloud_thetas_neg, R_neg_kpc, c="blue", label='Negative Velocities (Blue-shifted)')
+plt.scatter(cloud_thetas_pos, R_pos_kpc, c="red", label='Positive Velocities (Red-shifted)')
+plt.title("Galaxy plot showing H cloud positions", va = 'bottom')
+plt.annotate('Sun', xy=(0, 0), xytext=(10, 10),
+            arrowprops=dict(facecolor='black', shrink=0.05, width=1), 
+            fontsize=18)
+plt.annotate('GC', xy=(0, 8.5), xytext=(5, 5),
+            arrowprops=dict(facecolor='black', shrink=0.05, width=1), 
+            fontsize=18)
+plt.legend(prop={'size': 14})
+plt.figure(figsize=(20,20))
+#plt.rcParams['figure.figsize']=(11,11)
+plot.set_theta_zero_location("S")
+#plt.rcParams['font.size']= 20
+plt.rcParams['axes.titlesize'] = 20
+plt.savefig('polar_distinction.png')
+plt.show()
