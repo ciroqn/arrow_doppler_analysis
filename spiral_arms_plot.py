c499e07272b4f17ca1f77a782a4358e6bd24c83c
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2021-12-15 20:11:25 +0000

    import packages and establish constants

--- spiral_arms_plot.py
+++ spiral_arms_plot.py
@@ -1,1 +1,14 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
+
+# Establish necessary variables and constants for calculations to come
+
+R0 = 8.5 # sun distance from GC
+V0 = 220# sun rotation
+V = 244.35 # galaxy rotation
+KPC_M = 3.08567758e+19 # kiloparsec to metre conversion factor
+KPC_KM = 3.08567758e+16 # kpc to km conversion factor
+R0_KM = R0*KPC_KM # R0 in km
+R0_M = R0 * KPC_M # R0 in m
