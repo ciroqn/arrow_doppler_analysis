e13ac4e24c0a6ac4f19eaed405985440cfa4b581
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2021-12-15 20:08:06 +0000

    create df by reading archive data

--- read_archive_rotation_curve.py
+++ read_archive_rotation_curve.py
@@ -1,1 +1,7 @@
+# Reading archive data to read max. positive velocities and then displaying each graph at longitudes 0 -90 
 
+import pandas as pd
+
+df = pd.read_csv('archive_data.csv', sep=',', header=1, skiprows=[2],skipfooter=2, usecols=range(11), engine='python')
+
+df
