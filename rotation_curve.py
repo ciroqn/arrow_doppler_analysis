ddc9cf1349fd9f4a660e981305d64d67179ce86b
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2021-12-15 17:48:16 +0000

    def and functions for calculations

--- rotation_curve.py
+++ rotation_curve.py
@@ -3,3 +3,18 @@ import pandas as pd
 df = pd.read_csv('vobs_archive_21.csv', sep=',',skipfooter=11, usecols=range(3), engine='python')
 
 df
+
+
+################################ CREATE FUNCTIONS/DEFS FOR CALCULATIONS ###############################
+import numpy as np
+from astropy import constants as c
+from astropy import units as u
+
+def vel(v_max, angle, v_0 = 220):
+    velocity = v_max + v_0*np.sin(angle)
+    return velocity
+
+def radius(gamma, r_sun_kpc = 8.5):
+    gamma_rads = gamma*(np.pi/180)
+    radius = r_sun_kpc*np.sin(gamma_rads)
+    return radius
